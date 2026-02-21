8e2230311b071f4b9795e0446d6b3fe3c03ca89b
Darshancoder247 <darshankumar52262@gmail.com>
2026-02-21 15:15:32 +0530

    Time: 15 ms (52.41%) | Memory: 47.7 MB (66.89%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,23 +5,21 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer,Integer>> minHeap=new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            minHeap.offer(entry);
-            if (minHeap.size() > k) {
-                minHeap.poll(); // remove least frequent
-            }
-        }
-
-
-
-        int res[]=new int[k];
-        int i=0;
-        while(!minHeap.isEmpty())
+    PriorityQueue<Map.Entry<Integer,Integer>> minHeap=new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
+    for(Map.Entry<Integer,Integer> entry:map.entrySet())
+    {
+        minHeap.offer(entry);
+        if(minHeap.size()>k)
         {
-            res[i++]=minHeap.poll().getKey();
+            minHeap.poll();
         }
-      return res;
+    }
+    int res[]=new int[k];
+    int i=0;
+    while(!minHeap.isEmpty())
+    {
+        res[i++]=minHeap.poll().getKey();
+    }
+    return res;
     }
 }
